296949a8a1ebae23503dd07cb96cc52625c2b4c9
Alessio Raviola <alessio.raviola@codeploy.it>
2025-02-25 17:51:47 +0100

    refactor: move parsing into board

--- src/chess/board.zig
+++ src/chess/board.zig
@@ -212,6 +212,42 @@ pub const Board = struct {
         b.history.deinit();
     }
 
+    pub fn parseMove(b: *Board, uci: []const u8) ChessError!ChessMove {
+        if (uci.len != 4 and uci.len != 5) {
+            return ChessError.InvalidMove;
+        }
+
+        const from = try Square.fromAlgebraic(uci[0..2]);
+        const to = try Square.fromAlgebraic(uci[2..4]);
+        const promotion = if (uci.len == 5) try pieces.from(uci[5]) else pieces.NONE;
+
+        if (promotion != pieces.NONE and !pieces.isPromotionPiece(promotion)) {
+            return ChessError.InvalidPromotionPiece;
+        }
+
+        var ml = MoveList(ChessMove).new();
+
+        // Deterine if the move is pseudo legal
+        b.isKingAttacked(
+            b.state.current_side,
+            constants.MoveType.All,
+            b,
+            &ml,
+        );
+
+        var i: usize = 0;
+        while (i < ml.count) : (i += 1) {
+            var current = ml.list[i];
+            // If the move from, to and promotion are the same
+            // then we have found a valid move
+            if (current.from() == from.x and current.to() == to.x and current.promotion() == promotion) {
+                return current;
+            }
+        }
+        // We have found nothing, the move is not legal
+        return ChessError.NonPseudolegalMove;
+    }
+
     pub fn regenerateZobrist(b: *Board) void {
         var z: u64 = 0;
         // Pieces
